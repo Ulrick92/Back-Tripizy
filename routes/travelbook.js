2eed208da2d7866146095bec3d30498d086c393b
Admin <admin@admins-air-2.lan>
2018-12-20 16:10:11 +0100

    ajout console log

--- routes/travelbook.js
+++ routes/travelbook.js
@@ -65,6 +65,7 @@ router.get("/title/:title", isAuthenticated, (req, res) => {
   console.log("check title routes");
   for (let i = 0; i < req.user.travelbooks.length; i++) {
     if (req.user.travelbooks[i].title === req.params.title) {
+      console.log("error");
       return res.status(400).json({
         error: "Ce titre existe déjà."
       });
